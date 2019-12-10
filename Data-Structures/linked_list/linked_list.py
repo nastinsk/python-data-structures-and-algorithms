514767b9126548a83bd57d70d2fdcd7941fa85bf
nastinsk <nastinsk@gmail.com>
2019-12-09 16:00:05 -0800

    insert function created, test passed

--- Data-Structures/linked_list/linked_list.py
+++ Data-Structures/linked_list/linked_list.py
@@ -28,21 +28,42 @@ class LinkedList:
     def append(self, value):
         current = self.head
         while current:
+
+            print(current.value)
             if current.next == None:
                 current.next = Node(value)
                 return self.__str__()
             else:
                 current = current.next
 
+        self.head = Node(value)
+        return self.__str__()
+
     def __str__(self):
         list_str = ''
         current = self.head
         while current:
+            # print(current, "current")
             list_str += str(current.value ) + ', '
             current = current.next
         return list_str[:-2]
 
+    def insert_before(self, value, new_value):
+        if self.includes(value):
+            current = self.head
+            previous = current
+            while current:
+                if current.value == value:
+                    node = Node(new_value)
+                    node.next = current
+                    previous.next = node
+                    return self.__str__()
+                previous = current
+                current = current.next
+        else:
+            return 'Value is not in the list'
 
 
 
-
+test_list = LinkedList()
+test_list.append(9)
--- Data-Structures/linked_list/test_linked_list_day2.py
+++ Data-Structures/linked_list/test_linked_list_day2.py
@@ -5,5 +5,22 @@ values = ['a', 'b', 4, 'd']
 for el in values:
     linked_list.insert(el)
 
+
 def test_append():
     assert "d, 4, b, a, z" == linked_list.append('z')
+
+def test_append_to_empty():
+    linked_list = LinkedList()
+    assert "z" == linked_list.append('z')
+
+def test_insert_before_value_in_list():
+
+    assert "d, 4, b, 3, a, z" == linked_list.insert_before('a', 3)
+
+def test_insert_before_value_not_in_list():
+
+    assert 'Value is not in the list' == linked_list.insert_before('e', 3)
+
+def test_assert_to_empty_list():
+    linked_list = LinkedList()
+    assert 'Value is not in the list' == linked_list.insert_before('e', 3)
