d5f3215814928772fbebace06b8cac0ae1859596
nastinsk <nastinsk@gmail.com>
2019-12-11 23:37:15 -0800

    made solution code more DRY

--- challenges/ll_merge/ll_merge.py
+++ challenges/ll_merge/ll_merge.py
@@ -44,74 +44,55 @@ class LinkedList:
         return length
 
 
-def merge_list(list1, list2):
+
+def merge_list2(list1, list2):
     """
     Merge_lists function which takes two linked lists as arguments. Zips them together into one so that the nodes alternate between the two lists and return a reference to the head of the zipped list.
     """
 
-    list1_length = list1.length_()
-    list2_length = list2.length_()
+    current1 = list1.head
+    current2 = list2.head
 
-    if list1_length == 0 and list2_length == 0:
+    if current1 == None and current2 == None:
         raise Exception("lists are empty")
-    if list1_length == 0:
+
+    if not current1:
         list1.head = list2.head
         return list1.head
-    if list2_length == 0:
+
+    if not current2:
         return list1.head
 
-    current1 = list1.head  #current1.val == a
-    current2 = list2.head   #current2.val == e
-    temp = current2.next    #temp.val - f temp.next -> g ->h
+    temp = current2.next
 
-
-    if list1_length > list2_length:
-        for _ in range(list2_length-1):
-            current1.next, current2.next = current2, current1.next
-            current1 = current2.next
-            current2, temp = temp, temp.next
-
+    while current1.next and current2.next:
         current1.next, current2.next = current2, current1.next
+        current1 = current2.next
+        current2, temp = temp, temp.next
 
-
-        return list1.head
-
-
-    if list1_length < list2_length:
-        for _ in range(list1_length-1):
-            current1.next, current2.next = current2, current1.next
-            current1 = current2.next
-            current2, temp = temp, temp.next
-
+    if not current1.next:
         current1.next = current2
         return list1.head
 
-    else:
-        for _ in range(list1_length-1):
-            current1.next, current2.next = current2, current1.next
-            current1 = current2.next
-            current2, temp = temp, temp.next
-        current1.next = current2
-
+    if not current2.next:
+        current1.next, current2.next = current2, current1.next
         return list1.head
 
 
 
 def create_list(vals):
-    """helperfunction to create list with given values,, used in test module"""
+    """helper function to create list with given values, used in test module"""
     my_list = LinkedList()
     for i in vals:
         my_list.insert(i)
     return my_list
 
-l_list1 = create_list(['d', 'c', 'b', 'a'])
-l_list2 = create_list(['h','g','f','e', 'z'])
-ref_to_head = merge_list(l_list1, l_list2)
-print(ref_to_head.value)
 
 if __name__ == "__main__":
 
-    l_list1 = create_list(['c', 'b', 'a', 1, 4])
-    l_list2 = create_list(['g','f','e'])
+    l_list1 = create_list(['d', 'c', 'b', 'a'])
+    l_list2 = create_list(['h','g','f','e', 'z'])
+    ref_to_head = merge_list2(l_list1, l_list2)
+    print(ref_to_head.value)
 
-    print(merge_list(l_list1, l_list2))
+
