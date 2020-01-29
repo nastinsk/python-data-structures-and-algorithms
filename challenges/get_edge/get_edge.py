cbd8587215fd59cdf618e5bbda73d7608af2dd32
nastinsk <nastinsk@gmail.com>
2020-01-28 23:43:28 -0800

    docstrings added

--- challenges/get_edge/get_edge.py
+++ challenges/get_edge/get_edge.py
@@ -1,18 +1,21 @@
 from graph import Graph
 
 def helper(lst_nodes):
+    """Helper function to convert list of nodes to dictionary with string type keys"""
     dict_nodes = {}
     for node in lst_nodes:
         dict_nodes[node.value] = node
     return dict_nodes
 
 def helper2(lst_nodes):
+    """Helper function to convert given list of neighbor nodes to dictionary with string type keys"""
     neighbors = {}
     for node in lst_nodes:
         neighbors[node[0].value] = node
     return neighbors
 
 def get_edge(graph, arr):
+    """Function that takes in graph which represent possble trips between cities and their costs and array of city names. Returns True or False based on whether the full trip is possible with direct flights, and how much it would cost."""
 
 
     dict_nodes = helper(graph.get_nodes())
